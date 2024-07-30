c3aaec94cf1e8736f1d93689fa6affdb99b53053
Railey Macbook Pro <raileyvictuelles@gmail.com>
2024-07-31 03:59:13 +0800

    neovim: fixed sorting in `nvim-tree` - Fixed sorting of directories and files - Added numerical sorting functionality

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -22,7 +22,6 @@ local function on_attach(bufnr)
     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
-
   -- Default mappings. Feel free to modify or remove as you wish.
   --
   -- BEGIN_DEFAULT_ON_ATTACH
@@ -107,6 +106,7 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
   on_attach = on_attach,
   sort_by = "case_sensitive",
+
   view = {
     adaptive_size = true,
   },
@@ -127,4 +127,48 @@ vim.api.nvim_create_autocmd('BufEnter', {
    nested = true,
 })
 
+local sort_by_name = true
+local function sort_by_natural(nodes)
+	local function sorter(left, right)
+		if left.type ~= "directory" and right.type == "directory" then
+			return false
+		elseif left.type == "directory" and right.type ~= "directory" then
+			return true
+		end
+		left = left.name:lower()
+		right = right.name:lower()
 
+		if left == right then
+			return false
+		end
+
+		for i = 1, math.max(string.len(left), string.len(right)), 1 do
+			local l = string.sub(left, i, -1)
+			local r = string.sub(right, i, -1)
+
+			if
+				type(tonumber(string.sub(l, 1, 1))) == "number"
+				and type(tonumber(string.sub(r, 1, 1))) == "number"
+			then
+				local l_number = tonumber(string.match(l, "^[0-9]+"))
+				local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+				if l_number ~= r_number then
+					return l_number < r_number
+				end
+			elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+				return l < r
+			end
+		end
+	end
+
+	if sort_by_name then
+		table.sort(nodes, sorter)
+	else
+		return "modification_time"
+	end
+end
+
+require("nvim-tree").setup({
+	sort_by = sort_by_natural,
+})
