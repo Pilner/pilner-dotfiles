b933fd36fb15032c65bf71b316717f6af99dedc5
Fabian Railey Victuelles <raileyvictuelles@gmail.com>
2024-08-13 09:01:59 +0800

    nvim: set aside old `vim-plug` config file

--- nvim/after/plugin/single-compile.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<F9>", ":SCCompile<cr>")
-vim.keymap.set("n", "<F10>", ":SCCompileRun<cr>")
--- nvim/after/plugin/tip.lua
+++
@@ -1,5 +0,0 @@
-require("tip").setup({
-	seconds = 2,
-	title = "Tip!",
-	url = "https://vtip.43z.one", -- Or https://vimiscool.tech/neotip
-})
