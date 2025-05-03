0e5b83d132dc2b96115cb46ad8f4b1aa110778ab
Railey Macbook Pro <raileyvictuelles@gmail.com>
2025-05-03 18:59:40 +0800

    feat: changes

--- nvim/lua/railey/plugins/cosmetics/tip.lua
+++ nvim/lua/railey/plugins/cosmetics/tip.lua
@@ -1,13 +1,13 @@
 return {
-	"TobinPalmer/Tip.nvim",
-	event = "VimEnter",
-	init = function()
-		-- Default config
-		--- @type Tip.config
-		require("tip").setup({
-			seconds = 2,
-			title = "Tip!",
-			url = "https://vtip.43z.one", -- Or https://vimiscool.tech/neotip
-		})
-	end,
+	-- "TobinPalmer/Tip.nvim",
+	-- event = "VimEnter",
+	-- init = function()
+	-- 	-- Default config
+	-- 	--- @type Tip.config
+	-- 	require("tip").setup({
+	-- 		seconds = 2,
+	-- 		title = "Tip!",
+	-- 		url = "https://vtip.43z.one", -- Or https://vimiscool.tech/neotip
+	-- 	})
+	-- end,
 }
--- nvim/lua/railey/plugins/editing/coc.lua
+++ nvim/lua/railey/plugins/editing/coc.lua
@@ -148,6 +148,6 @@ return {
 		-- Add (Neo)Vim's native statusline support
 		-- NOTE: Please see `:h coc-status` for integrations with external plugins that
 		-- provide custom statusline: lightline.vim, vim-airline
-		vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}")
+		-- vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}")
 	end,
 }
