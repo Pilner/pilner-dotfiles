f8524bf6eff73f7827b2e78d25bc707ff38e90d9
Railey Victuelles M4 Air <raileyvictuelles@gmail.com>
2026-05-13 05:57:22 +0800

    fix: treesitter bug and add html support

--- config_files/.config/nvim/lua/railey/plugins/utility.lua
+++ config_files/.config/nvim/lua/railey/plugins/utility.lua
@@ -238,7 +238,13 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "yioneko/nvim-yati",
+    },
     build = ":TSUpdate",
+    branch = "master",
+    event = { "BufReadPre", "BufNewFile" }, -- Load treesitter early for other plugins
+    main = "nvim-treesitter.configs",       -- Tells lazy.nvim exactly which module to require
     opts = {
       ensure_installed = {
         "c",
@@ -253,7 +259,28 @@ return {
         "bash",
         "markdown",
       },
-    },
+      highlight = {
+        enable = true
+      },
+      indent = {
+        enable = false
+      },
+      autotag = {
+        enable = true
+      },
+      yati = {
+        enable = true
+      },
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        -- vim.uv is the modern replacement for vim.loop
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+      additional_vim_regex_highlighting = false,
+    }
   },
   {
     "mbbill/undotree",
