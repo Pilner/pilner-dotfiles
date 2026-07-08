489d10f79095318b8f08e5a2a16ae40190400354
Railey Victuelles M4 Air <raileyvictuelles@gmail.com>
2026-07-09 03:04:57 +0800

    fix: use native treesitter - remove `nvim-treesitter` plugin - use `tree-sitter-manager` to handle parsers

---
+++ config_files/.config/nvim/lua/railey/plugins/utility/tree-sitter-manager.lua
@@ -0,0 +1,23 @@
+return {
+  "romus204/tree-sitter-manager.nvim",
+  dependencies = {},
+  config = function()
+    require("tree-sitter-manager").setup({
+      auto_install = true,
+      highlight = true,
+      ensure_installed = {
+        "c",
+        "go",
+        "tsx",
+        "json",
+        "javascript",
+        "typescript",
+        "python",
+        "lua",
+        "vim",
+        "bash",
+        "markdown",
+      },
+    })
+  end,
+}
