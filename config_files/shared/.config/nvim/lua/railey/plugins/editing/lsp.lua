21c4b12717ba06f040a15f534afd5bb03fede489
pdax-fabianvictuelles <fabian.victuelles@pdax.ph>
2026-09-07 18:06:59 +0800

    fix: fold bug

--- config_files/shared/.config/nvim/lua/railey/plugins/editing/lsp.lua
+++ config_files/shared/.config/nvim/lua/railey/plugins/editing/lsp.lua
@@ -1,94 +1,106 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- Enable showing errors/warnings while in insert mode
-    vim.diagnostic.config({
-      update_in_insert = true,
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		-- Enable showing errors/warnings while in insert mode
+		vim.diagnostic.config({
+			update_in_insert = true,
+		})
 
-    vim.api.nvim_create_autocmd("CursorHold", {
-      callback = function()
-        vim.diagnostic.open_float(nil, {
-          focusable = false, -- Prevents your cursor from jumping into the window
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          source = "always", -- Shows which LSP sent the error (e.g., gopls)
-          prefix = " ",
-        })
-      end,
-    })
+		vim.api.nvim_create_autocmd("CursorHold", {
+			callback = function()
+				vim.diagnostic.open_float(nil, {
+					focusable = false, -- Prevents your cursor from jumping into the window
+					close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+					source = "always", -- Shows which LSP sent the error (e.g., gopls)
+					prefix = " ",
+				})
+			end,
+		})
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      desc = "LSP actions",
-      callback = function(event)
-        local opts = { buffer = event.buf }
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-        vim.keymap.set("n", "gd", function() require("snacks").picker.lsp_definitions() end, { desc = "Go to Definition" }, opts)
-        vim.keymap.set("n", "gD", function() require("snacks").picker.lsp_declarations() end, { desc = "Go to Declaration" }, opts)
-        vim.keymap.set("n", "gi", function() require("snacks").picker.lsp_implementations() end, { desc = "Go to Implementation" }, opts)
-        vim.keymap.set("n", "gt", function() require("snacks").picker.lsp_type_definitions() end, { desc = "Go to Type Definition" }, opts)
-        vim.keymap.set("n", "gr", function() require("snacks").picker.lsp_references() end, { desc = "Go to References" }, opts)
-        vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-        vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-        vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-        vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function(event)
+				local opts = { buffer = event.buf }
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				client.server_capabilities.semanticTokensProvider = nil
+				vim.keymap.set("n", "gd", function()
+					require("snacks").picker.lsp_definitions()
+				end, { desc = "Go to Definition" }, opts)
+				vim.keymap.set("n", "gD", function()
+					require("snacks").picker.lsp_declarations()
+				end, { desc = "Go to Declaration" }, opts)
+				vim.keymap.set("n", "gri", function()
+					require("snacks").picker.lsp_implementations()
+				end, { desc = "Go to Implementation" }, opts)
+				vim.keymap.set("n", "grt", function()
+					require("snacks").picker.lsp_type_definitions()
+				end, { desc = "Go to Type Definition" }, opts)
+				vim.keymap.set("n", "grr", function()
+					require("snacks").picker.lsp_references()
+				end, { desc = "Go to References" }, opts)
+				vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+				vim.keymap.set({ "n", "x" }, "<F3>", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+				vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
 
-        vim.keymap.set("n", "]d", function()
-          vim.diagnostic.goto_next()
-        end, { desc = "Jump to next diagnostic" }, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_next()
+				end, { desc = "Jump to next diagnostic" }, opts)
 
-        vim.keymap.set("n", "[d", function()
-          vim.diagnostic.goto_prev()
-        end, { desc = "Jump to previous diagnostic" }, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_prev()
+				end, { desc = "Jump to previous diagnostic" }, opts)
 
-        vim.keymap.set("n", "]e", function()
-          vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-        end, { desc = "Jump to next error" }, opts)
+				vim.keymap.set("n", "]e", function()
+					vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+				end, { desc = "Jump to next error" }, opts)
 
-        vim.keymap.set("n", "[e", function()
-          vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
-        end, { desc = "Jump to previous error" }, opts)
+				vim.keymap.set("n", "[e", function()
+					vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+				end, { desc = "Jump to previous error" }, opts)
 
-        vim.keymap.set("n", "]w", function()
-          vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
-        end, { desc = "Jump to next warning" }, opts)
+				vim.keymap.set("n", "]w", function()
+					vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+				end, { desc = "Jump to next warning" }, opts)
 
-        vim.keymap.set("n", "[w", function()
-          vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
-        end, { desc = "Jump to previous warning" }, opts)
-      end,
-    })
+				vim.keymap.set("n", "[w", function()
+					vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+				end, { desc = "Jump to previous warning" }, opts)
+			end,
+		})
 
-    require("mason").setup()
+		require("mason").setup()
 
-    -- Tell our LSP servers that we support advanced autocomplete capabilities
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		-- Tell our LSP servers that we support advanced autocomplete capabilities
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    require("mason-lspconfig").setup({
-      ensure_installed = { "gopls" },
+		require("mason-lspconfig").setup({
+			ensure_installed = { "gopls" },
 
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            -- Pass the autocomplete capabilities to every server Mason installs
-            capabilities = capabilities,
-          })
-        end,
-        ["gopls"] = function()
-          require("lspconfig").gopls.setup({
-            capabilities = capabilities,
-            settings = {
-              gopls = {
-                gofumpt = true, -- Tells gopls to use gofumpt instead of standard gofmt
-              },
-            },
-          })
-        end,
-      },
-    })
-  end,
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						-- Pass the autocomplete capabilities to every server Mason installs
+						capabilities = capabilities,
+					})
+				end,
+				["gopls"] = function()
+					require("lspconfig").gopls.setup({
+						capabilities = capabilities,
+						settings = {
+							gopls = {
+								gofumpt = true, -- Tells gopls to use gofumpt instead of standard gofmt
+							},
+						},
+					})
+				end,
+			},
+		})
+	end,
 }
--- config_files/shared/.config/nvim/lua/railey/plugins/utility/nvim-ufo.lua
+++ config_files/shared/.config/nvim/lua/railey/plugins/utility/nvim-ufo.lua
@@ -1,82 +1,90 @@
 return {
-  "kevinhwang91/nvim-ufo",
-  dependencies = {
-    "kevinhwang91/promise-async",
-    {
-      "luukvbaal/statuscol.nvim",
-      config = function()
-        -- 1. Neovim options required for UFO folding
-        vim.o.foldcolumn = "1"
-        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-        vim.o.foldlevel = 99
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+		{
+			"luukvbaal/statuscol.nvim",
+			config = function()
+				-- 1. Neovim options required for UFO folding
+				vim.o.foldcolumn = "1"
+				vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+				vim.o.foldlevel = 99
+				vim.o.foldlevelstart = 99
+				vim.o.foldenable = true
 
-        local builtin = require("statuscol.builtin")
+				local builtin = require("statuscol.builtin")
 
-        -- 2. Combined statuscol setup
-        require("statuscol").setup({
-          setopt = true,
-          relculright = true,
-          segments = {
-            -- Segment 1: UFO Fold column
-            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+				-- 2. Combined statuscol setup
+				require("statuscol").setup({
+					setopt = true,
+					relculright = true,
+					segments = {
+						-- Segment 1: UFO Fold column
+						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 
-            -- Segment 2: Git signs
-            {
-              sign = {
-                namespace = { "gitsigns.*" },
-                name = { "gitsigns.*" },
-              },
-              click = "v:lua.ScSa",
-            },
+						-- Segment 2: Git signs
+						{
+							sign = {
+								namespace = { "gitsigns.*" },
+								name = { "gitsigns.*" },
+							},
+							click = "v:lua.ScSa",
+						},
 
-            -- Segment 3: All other signs (LSP Diagnostics, etc.)
-            {
-              sign = {
-                namespace = { ".*" },
-                name = { ".*" },
-                maxwidth = 1,
-                colwidth = 1,
-                auto = false,
-              },
-              click = "v:lua.ScSa",
-            },
+						-- Segment 3: All other signs (LSP Diagnostics, etc.)
+						{
+							sign = {
+								namespace = { ".*" },
+								name = { ".*" },
+								maxwidth = 1,
+								colwidth = 1,
+								auto = false,
+							},
+							click = "v:lua.ScSa",
+						},
 
-            -- Segment 4: Custom absolute and relative line numbers
-            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-          },
-        })
-      end,
-    },
-  },
-  event = "BufReadPost",
-  opts = {
-    provider_selector = function()
-      return { "treesitter", "indent" }
-    end,
-  },
-  init = function()
-    -- Keymaps for UFO
-    vim.keymap.set("n", "zR", function()
-      require("ufo").openAllFolds()
-    end)
-    vim.keymap.set("n", "zM", function()
-      require("ufo").closeAllFolds()
-    end)
-    vim.keymap.set("n", "H", "za", {
-      remap = true,
-      silent = true,
-      desc = "Toggle Fold (using za)",
-    })
+						-- Segment 4: Custom absolute and relative line numbers
+						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					},
+				})
+			end,
+		},
+	},
+	event = "BufReadPost",
+	opts = {
+		provider_selector = function()
+			return { "treesitter", "indent" }
+		end,
+	},
+	init = function()
+		-- Keymaps for UFO
+		vim.keymap.set("n", "zR", function()
+			require("ufo").openAllFolds()
+      vim.wo.foldlevel = 99
+		end)
+		vim.keymap.set("n", "zM", function()
+			require("ufo").closeAllFolds()
+		end)
+		vim.keymap.set("n", "H", "za", {
+			remap = true,
+			silent = true,
+			desc = "Toggle Fold (using za)",
+		})
 
-    -- Hover definition (Note: I updated the fallback here for native LSP)
-    vim.keymap.set("n", "K", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
-      if not winid then
-        -- Since you set up nvim-lspconfig earlier, this triggers standard LSP hover
-        vim.lsp.buf.hover()
-      end
-    end)
-  end,
+		-- Add mappings for z1 through z9 to set foldlevel
+		for i = 1, 9 do
+			vim.keymap.set("n", "z" .. i, function()
+				require("ufo").closeFoldsWith(i - 1)
+			end, { desc = "Fold up to level " .. i })
+		end
+
+		-- Hover definition (Note: I updated the fallback here for native LSP)
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				-- Since you set up nvim-lspconfig earlier, this triggers standard LSP hover
+				vim.lsp.buf.hover()
+			end
+		end)
+	end,
 }
